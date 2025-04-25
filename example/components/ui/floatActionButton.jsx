b2b998592d13392d24b37e05398cb1173c7bcda7
emreertugrl <emrertugrl@gmail.com>
2025-04-25 18:28:49 +0300

    float action button defined to UI

---
+++ example/components/ui/floatActionButton.jsx
@@ -0,0 +1,28 @@
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import {themeColors} from '../../themes/themeColors';
+import {Add} from 'iconsax-react-nativejs';
+
+const FloatActionButton = () => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Add size={40} color={themeColors.WHITE} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: themeColors.GREEN,
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    position: 'absolute',
+    bottom: 50,
+    right: 20,
+  },
+});
+
+//make this component available to the app
+export default FloatActionButton;
