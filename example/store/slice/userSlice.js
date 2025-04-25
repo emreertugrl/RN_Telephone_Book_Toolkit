384844f71aa640dfac35ddd87ba6be732eae3984
emreertugrl <emrertugrl@gmail.com>
2025-04-25 19:54:02 +0300

    userReducer fixed

--- example/store/slice/userSlice.js
+++ example/store/slice/userSlice.js
@@ -1,19 +1,7 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-  users: [
-    {
-      id: 1,
-      name: 'Ahmet',
-      surname: 'Siyah',
-      age: 24,
-      address: 'İstanbul/Kadıköy',
-      phone: '5555555555',
-      email: 'test@gmail.com',
-      profileImage: '',
-      gender: 'Erkek',
-    },
-  ],
+  users: [],
 };
 const userSlice = createSlice({
   name: 'Users',
