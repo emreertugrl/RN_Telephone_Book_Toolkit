456187211b9462be57813d761a28d35f8ee5504c
emreertugrl <emrertugrl@gmail.com>
2025-04-25 19:54:23 +0300

    defaultScreenStyle fixed

--- example/styles/defaultScreenStyle.js
+++ example/styles/defaultScreenStyle.js
@@ -1,9 +1,11 @@
 import {StyleSheet} from 'react-native';
+import {themeColors} from '../themes/themeColors';
 
 const defaultScreenStyle = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
+    backgroundColor: themeColors.WHITE,
   },
 });
 
