a85a81886b2851faa63c14976bf971a7b6a5e2c2
emreertugrl <emrertugrl@gmail.com>
2025-04-25 18:35:58 +0300

    random color and theme color add

--- example/themes/themeColors.js
+++ example/themes/themeColors.js
@@ -2,6 +2,7 @@ const themeColors = {
   WHITE: '#FFFFFF',
   BLACK: '#000000',
   GRAY: '#B2B2B2',
+  GREEN: '#37D67A',
 };
 
 export {themeColors};
--- example/utils/functions.js
+++ example/utils/functions.js
@@ -7,4 +7,12 @@ const getInitialNameSurname = (name, surname) => {
   const surnameInitial = surname[0].charAt().toUpperCase();
   return nameInitial + surnameInitial;
 };
-export {compareName, getInitialNameSurname};
+
+const getRandomColor = () => {
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = 70 + Math.random() * 30; // %70-100 arası
+  const lightness = 70 + Math.random() * 10; // %70-80 arası
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+};
+
+export {compareName, getInitialNameSurname, getRandomColor};
